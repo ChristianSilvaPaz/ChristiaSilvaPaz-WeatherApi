f2a46dfc1b6fc0d76456fc27266def843cec5f31
ChristianSilvaPaz <christiangunner@hotmail.com>
2023-10-12 14:21:52 -0300

    Adicionar WeatherCityTest

--- WeatherApi.Tests/Entities/WeatherCityTest.cs
+++ WeatherApi.Tests/Entities/WeatherCityTest.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using WeatherApi.Entities;
 
-namespace WeatherApi.Tests.Entities
+namespace WeatherApi.Tests.Entities;
+
+public class WeatherCityTest
 {
-    internal class WeatherCityTest
+    [Fact]
+    public async Task WeatherCity_ShouldReturnObjectWeatherCity()
     {
+        //Arrange
+        Random rand = new Random();
+
+        string name = "Paracatu";
+        double temperature = rand.NextDouble();
+        double minimumTemperature = rand.NextDouble();
+        double maximumTemperature = rand.NextDouble();
+        decimal latitude = (decimal)rand.NextDouble();
+        decimal longitude = (decimal)rand.NextDouble();
+
+        //Act
+        var entity = new WeatherCity(
+            name,
+            temperature,
+            minimumTemperature,
+            maximumTemperature,
+            latitude,
+            longitude);
+
+        //Assert
+        Assert.True(entity.Id == 0);
+        Assert.Equal(entity.Name, name);
+        Assert.Equal(entity.Temperature, temperature);
+        Assert.Equal(entity.MinimumTemperature, minimumTemperature);
+        Assert.Equal(entity.MaximumTemperature, maximumTemperature);
+        Assert.Equal(entity.Latitude, latitude);
+        Assert.Equal(entity.Longitude, longitude);
+        Assert.NotEqual(entity.CreationDate, DateTime.Now);
     }
 }
